ab4f2d471a7656c63d2f33f5a263ef6603f0c0f5
jakesummer <jakersummer@gmail.com>
2026-03-17 18:31:52 -0400

    Implement getComputerChoice()

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    return (
+        (choice === 0) ? "rock":
+        (choice === 1) ? "paper":
+        "scissors"
+    );
+}
+
+getComputerChoice();
